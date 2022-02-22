7692596f9535046408d2c5879c17b1e5019b6139
Ethosa <social.ethosa@gmail.com>
2022-02-23 00:10:36 +0700

    remove unnecessary files

--- app/src/test/java/com/ethosa/ktc/UnitTest.kt
+++
@@ -1,9 +0,0 @@
-package com.ethosa.ktc
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class UnitTest {
-}
